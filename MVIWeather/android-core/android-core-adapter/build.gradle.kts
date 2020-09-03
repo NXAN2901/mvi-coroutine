e3c6d1843c8fcb6a67e823f8358623e59dfe7afc
ANNX <xuanan290192@gmail.com>
2020-09-03 23:44:27 +0700

    Update gradle script for androi core adapter
    Change gradle to kts file

---
+++ MVIWeather/android-core/android-core-adapter/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("com.android.library")
+    id("app-plugins")
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.3.1")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+}
