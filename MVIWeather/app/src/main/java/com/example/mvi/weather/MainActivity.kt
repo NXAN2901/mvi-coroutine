27685cee51990de032df0403d5ed23dc74b4b536
ANNX <xuanan290192@gmail.com>
2020-09-21 22:10:07 +0700

    Make Fullscreen App
    - Add Fullscreen style
    - Set Translucent status bar for activity

--- MVIWeather/app/src/main/java/com/example/mvi/weather/MainActivity.kt
+++ MVIWeather/app/src/main/java/com/example/mvi/weather/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.mvi.weather
 
+import android.app.Activity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        initStatusBar()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun initStatusBar() {
+        //make translucent statusBar on kitkat devices
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        setWindowFlag(this, on = true)
+        window.statusBarColor = Color.TRANSPARENT
+    }
+
+    private fun setWindowFlag(activity: Activity, bits: Int = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, on: Boolean = false) {
+        val win = activity.window
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or bits
+        } else {
+            winParams.flags = winParams.flags and bits.inv()
+        }
+        win.attributes = winParams
     }
 }
