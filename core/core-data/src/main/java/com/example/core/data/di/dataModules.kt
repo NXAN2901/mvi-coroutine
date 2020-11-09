f81081abae1eaf2f3b34f4a10a3cb5b8940dfb2f
ANNX1 <xuanan290192@gmail.com>
2020-11-09 21:39:17 +0700

    Update dataModules.kt
    Add DI for AppPreferencesRepo

--- core/core-data/src/main/java/com/example/core/data/di/dataModules.kt
+++ core/core-data/src/main/java/com/example/core/data/di/dataModules.kt
@@ -1,8 +1,21 @@
 package com.example.core.data.di
 
+import android.app.Application
 import com.example.core.data.datastore.AppFlagDataStore
+import com.example.core.data.repos.AppPreferencesRepo
+import com.example.core.data.repos.AppPreferencesRepoImpl
+import com.example.core.data.usecase.GetAppStateUseCase
+import com.example.core.data.usecase.UpdateAppStateUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import org.koin.dsl.module
 
+@FlowPreview
+@OptIn(ExperimentalCoroutinesApi::class)
 val dataModules = module {
     single { AppFlagDataStore(get()) }
+    single<AppPreferencesRepo> { AppPreferencesRepoImpl(get<Application>()) }
+
+    factory { GetAppStateUseCase(get(), get()) }
+    factory { UpdateAppStateUseCase(get(), get()) }
 }
